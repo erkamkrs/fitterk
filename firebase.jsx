021ee81f9ea738caf6fb8a6ea2e101906ea81dc4
Erkam Kiris <erkamkiris@gmail.com>
2023-11-12 23:35:19 +0300

    fix bugs

--- firebase.jsx
+++ firebase.jsx
@@ -4,12 +4,12 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 // Your web app's Firebase configuration
 export const firebaseApp = initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID
+  apiKey: "NEXT_PUBLIC_API_KEY",
+  authDomain: "fitterk-23.firebaseapp.com",
+  projectId: "fitterk-23",
+  storageBucket: "fitterk-23.appspot.com",
+  messagingSenderId: "207253210514",
+  appId: "1:207253210514:web:c8a4572c5ccf6028992d4b"
 });
 
 // Initialize Firebase
